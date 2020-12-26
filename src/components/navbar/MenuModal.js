026692d6860e76eb74ebf7a5fe1b95a143603965
Chin <chinchingastronomy@outlook.com>
2020-12-26 22:31:47 +0800

    removed call

--- src/components/navbar/MenuModal.js
+++ src/components/navbar/MenuModal.js
@@ -4,7 +4,7 @@ import logo from "../../assets/logo.jpg";
 import Modal from "@material-ui/core/Modal";
 import Backdrop from "@material-ui/core/Backdrop";
 import Fade from "@material-ui/core/Fade";
-import { AiOutlineClose } from "react-icons/ai";
+
 import Typography from "@material-ui/core/Typography";
 import Menu from "../menues/Menu";
 
