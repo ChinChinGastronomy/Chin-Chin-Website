d8cb0f379938d7f3d9215f88d83c8033d9ff34ff
Chin <chinchingastronomy@outlook.com>
2020-12-29 22:39:59 +0800

    deleted useRef

--- src/components/hero/NewHero.js
+++ src/components/hero/NewHero.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components/macro";
 import { slides } from "../../utils/utils";
 import { IoArrowForward, IoArrowBack } from "react-icons/io5";
