ed521e96c4dee4b187e8a11cff3579c756692200
Chin <76512555+ChinChinGastronomy@users.noreply.github.com>
2021-08-05 12:27:13 +0800

    Update AboutLeft.js

--- src/components/about section/AboutLeft.js
+++ src/components/about section/AboutLeft.js
@@ -5,7 +5,8 @@ const AboutLeft = () => {
     <div className="container-about">
       <div className="innerCtnAbout">
         <h1>Head chef</h1>
-        <h3>Chin To Hung</h3><br>
+        <h3>Chin To Hung</h3>
+        <br>
         <p>
           A real fine dining to me is about providing unique and memorable moments to customers, connecting the personality of the chef and restaurant to diners through the combinations of highest quality ingredients and great flavours, paired with good drinks and service.  Personality and unique enjoyable experiences are the keys, can be expressed in different styles, not necessarily always in the form of white table clothes, waiters in bow-ties and expensive bills
         </p>
